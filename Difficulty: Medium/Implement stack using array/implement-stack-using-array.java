825572e4802a825d51d46395f1cb11a14c2fd867
Mithuna Thirunagari <thirunagarimithuna1@gmail.com>
2026-02-26 11:56:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement stack using array/implement-stack-using-array.java
@@ -0,0 +1,47 @@
+class myStack {
+    int arr[];
+    int top;
+    int capacity;
+    public myStack(int n) {
+        // Define Data Structures
+       capacity = n;
+       arr = new int[n];
+       top=-1;
+    }
+
+    public boolean isEmpty() {
+        // check if the stack is empty
+        if(top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        // check if the stack is full
+        if(top == capacity-1){
+            return true;
+        }
+        return false;
+    }
+
+    public void push(int x) {
+        // Inserts x at the top of the stack
+        if(!isFull()){
+            arr[++top] = x;
+        }
+    }
+
+    public void pop() {
+        // Removes an element from the top of the stack
+        if(!isEmpty()){
+            top--;
+        }
+    }
+
+    public int peek() {
+        // Returns the top element of the stack
+        if(isEmpty()) return -1;
+        return arr[top];
+    }
+}
